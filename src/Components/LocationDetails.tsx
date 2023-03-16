7950061124f947e268802cc07fc63903bb0ea755
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2023-03-16 08:00:42 +0100

    added basic styles to location-details-component

--- src/Components/LocationDetails.tsx
+++ src/Components/LocationDetails.tsx
@@ -1,3 +1,22 @@
 export const LocationDetails = () => {
-	return <div>LocationDetails</div>;
+	return (
+		<div className="h-1/5 w-3/4 relative -translate-y-1/2 bg-white mx-auto flex justify-evenly items-center rounded-2xl">
+			<div className="flex flex-col">
+				<h3>Ip Address</h3>
+				<p>1111111111111111</p>
+			</div>
+			<div className="flex flex-col">
+				<h3>Location</h3>
+				<p>Brooklyn</p>
+			</div>
+			<div className="flex flex-col">
+				<h3>Timezone</h3>
+				<p>UTC</p>
+			</div>
+			<div className="flex flex-col">
+				<h3>ISP</h3>
+				<p>Starlink</p>
+			</div>
+		</div>
+	);
 };
