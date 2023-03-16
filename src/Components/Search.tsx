540ac437b624af70784ea118294e4c7b71f429d8
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2023-03-16 07:32:56 +0100

    added styles to search component

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,3 +1,19 @@
 export const Search = () => {
-	return <div className="h-1/5 bg-bg-desktop">SearchComponent</div>;
+	return (
+		<div className="h-1/3 bg-bg-desktop flex flex-col justify-start items-center">
+			<h1 className="text-2xl text-white font-semibold my-7">
+				IP Address Tracker
+			</h1>
+			<form className="w-3/5 flex flex-row justify-center">
+				<input
+					className="h-12 w-3/6 rounded-l-xl pl-4 placeholder:text-md"
+					type="text"
+					placeholder="Search for any IP address or domain"
+				/>
+				<button className="w-1/12 max-w-2 bg-black text-white rounded-r-xl">
+					&gt;
+				</button>
+			</form>
+		</div>
+	);
 };
