c3f322e9d3923fd5d23a3980ed100a3ceb43e277
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2023-03-19 20:01:22 +0100

    fixed label issue

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -4,11 +4,13 @@ export const Search = () => {
 			<h1 className="text-2xl text-white font-semibold my-7">
 				IP Address Tracker
 			</h1>
-			<form className="w-3/5 flex flex-row justify-center">
+			<form className="w-11/12 sm:w-3/5 flex flex-row justify-center">
+				<label htmlFor="ip"></label>
 				<input
-					className="h-12 w-3/6 rounded-l-xl pl-4 placeholder:text-md"
+					className="h-12 w-11/12 sm:w-3/6 rounded-l-xl pl-4 placeholder:text-md"
 					type="text"
 					placeholder="Search for any IP address or domain"
+					id="ip"
 				/>
 				<button className="w-1/12 max-w-2 bg-black text-white rounded-r-xl">
 					&gt;
