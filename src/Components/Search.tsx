2f3e4f0c0156fd24ffde747dc8afbcd03e454d41
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2023-03-20 20:27:41 +0100

    added axios to dependencies and also added axios to serach component

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,4 +1,27 @@
+import React, { useEffect, useState } from "react";
+import api_baseUrl from "../api/api_baseUrl";
+
 export const Search = () => {
+	const [ipAddress, setIpAddress] = useState("");
+
+	const apiKey: string = "at_5I9sUYamw5MsQycwQTbrB6gMIRezB";
+
+	useEffect(() => {
+		const fetchLocation = async () => {
+			try {
+				const response = await api_baseUrl.get(
+					`/country?apiKey=${apiKey}&ipAddress=${ipAddress}`
+				);
+				console.log(response.data);
+				console.log(ipAddress);
+			} catch (err: any) {
+				console.log(err.response.data);
+				console.log(err.response.status);
+			}
+		};
+		fetchLocation();
+	}, [ipAddress]);
+
 	return (
 		<div className="h-1/3 bg-bg-desktop flex flex-col justify-start items-center">
 			<h1 className="text-2xl text-white font-semibold my-7">
