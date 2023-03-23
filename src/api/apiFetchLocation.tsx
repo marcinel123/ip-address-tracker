799b75a248d8730be77de81998068f55487d623e
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2023-03-23 20:35:58 +0100

    removed console log

--- src/api/apiFetchLocation.tsx
+++ src/api/apiFetchLocation.tsx
@@ -3,10 +3,9 @@ import api_baseUrl from "./api_baseUrl";
 export const useFetchLocation = (ipAddress: string) => {
 	const fetchLocation = async () => {
 		try {
-			const response = await api_baseUrl.get(
+			await api_baseUrl.get(
 				`/country?apiKey=${process.env.REACT_APP_KEY}&ipAddress=${ipAddress}`
 			);
-			console.log(response.data);
 		} catch (err: unknown) {
 			if (err instanceof Error) {
 				console.log(err.message);
