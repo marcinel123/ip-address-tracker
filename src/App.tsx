0a351d2153bf7bead983f15ecffc42626c5d174a
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2023-04-14 18:55:28 +0200

    removed conditionally rendered Map and LocationDetails components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { PropagateLoader } from "react-spinners";
 import { LocationDetails } from "./Components/LocationDetails";
 import { Map } from "./Components/Map";
 import { Search } from "./Components/Search";
@@ -10,16 +9,10 @@ export const App = () => {
 	return (
 		<>
 			<Search setLocationData={setLocationData} />
-			{locationData ? (
-				<LocationDetails locationData={locationData} />
-			) : (
-				<PropagateLoader color="#36d7b7" />
-			)}
-			{locationData ? (
-				<Map locationData={locationData} />
-			) : (
-				<PropagateLoader color="#36d7b7" />
-			)}
+
+			<LocationDetails locationData={locationData} />
+
+			<Map locationData={locationData} />
 		</>
 	);
 };
