688753c1da065df5df54865674f83716515add53
ela26 ela26 <m.eljasiewicz@gmail.com>
2023-04-18 19:56:43 +0200

    added types for error. error catch state

--- src/api/apiFetchLocation.tsx
+++ src/api/apiFetchLocation.tsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import api_baseUrl from "./api_baseUrl";
 
 export const useFetchLocation = (ipAddress: string) => {
-	const [isError, setIsError] = useState({});
+	const [isError, setIsError] = useState<string | unknown>("");
 	const fetchLocation = async () => {
 		try {
+			setIsError("");
 			const response = await api_baseUrl.get(
 				`/country,city?apiKey=${process.env.REACT_APP_KEY}&ipAddress=${ipAddress}`
 			);
 			return response;
 		} catch (err: unknown) {
 			if (err) {
-				setIsError(err)
+				console.log(err.toJSON());
+				setIsError(err.toJSON());
+				console.log(typeof isError);
 			}
 		}
 	};
