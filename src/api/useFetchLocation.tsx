bdd653e76b26b7373a57a352431f278b89f2a687
ela26 ela26 <m.eljasiewicz@gmail.com>
2023-04-27 20:16:14 +0200

    fixes

--- src/api/useFetchLocation.tsx
+++ src/api/useFetchLocation.tsx
@@ -1,17 +1,18 @@
-import {  useState } from "react";
+import {  Dispatch, SetStateAction, useState } from "react";
 import api_baseUrl from "./api_baseUrl";
 
 export const useFetchLocation = (ipAddress: string) => {
-	const [locationData, setLocationData] = useState();
+	const [locationData, setLocationData] = useState<Dispatch<SetStateAction<undefined>> | undefined>();
 	console.log(locationData)
 	const [isApiErr, setApiErr] = useState<boolean>(false);
 	
 	const fetchLocation = async () => {
 		try {
 			setApiErr(false);
-			return await api_baseUrl.get(
+			const response = api_baseUrl.get(
 				`/country,city?apiKey=${process.env.REACT_APP_KEY}&ipAddress=${ipAddress}`
 			);
+			setLocationData(response)
 		} catch (err: unknown) {
 			setApiErr(true);	
 		}
